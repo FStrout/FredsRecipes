c5a61efafac40790a77fdb20d0c5d4d89a39b73b
Fred Strout <37194417+FStrout@users.noreply.github.com>
2025-01-16 12:42:35 -0600

    Project Creation

---
+++ FredsRecipes/FredsRecipes/FredsRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FredsRecipesApp.swift
+//  FredsRecipes
+//
+//  Created by Fred Strout on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct FredsRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
